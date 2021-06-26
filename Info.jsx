635916d2e8be5a732e6dd247a92be41d4cf0abca
yuvi-07 <86522100+yuvi-07@users.noreply.github.com>
2021-06-26 18:47:45 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div class="note">
+      <h1>Javascript and React.js</h1>
+      <p> A basic web dev ReactJs Bootcamp</p>
+    </div>
+  );
+}
+
+export default Info;
